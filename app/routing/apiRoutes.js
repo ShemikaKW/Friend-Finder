f472a45f872168a104243e9b95c0ed60927944e2
ShemikaKW <Shemikakw@gmail.com>
2018-11-15 18:03:23 -0500

    Final commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,8 @@
 var friends = require("../data/friends");
 
+
+
+
 // ROUTES
 function apiRoutes(app) {
     app.get("/api/friends", function (req, res) {
@@ -7,9 +10,31 @@ function apiRoutes(app) {
     });
     app.post("/api/friends", function (req, res) {
         var jsonDomPost = req.body;
-        console.log(jsonDomPost);
+        var bestFriendindex = " ";
+        var bestFriendNum = 100;
+        var userScores = jsonDomPost.scores;
+        var difference = 0;
+
+        //Logic to find match
+        for (var i = 0; i < friends.length; i++){
+            
+            var currentFriendScores = friends[i].scores;
+            difference = 0;
+
+            for ( var j = 0; j < currentFriendScores.length; j++){
+                var friendCurrentScore = currentFriendScores[j];
+                var userCurrentScore = userScores[j];
+
+                difference += Math.abs(parseInt(friendCurrentScore) - parseInt(userCurrentScore));
+            }
+            if (difference < bestFriendNum){
+                bestFriendNum = difference;
+                bestFriendIndex = i;
+            }
+        }
+
         friends.push(jsonDomPost);
-        res.json(jsonDomPost);
+        res.json(friends[bestFriendIndex]);
     });
 }
 
